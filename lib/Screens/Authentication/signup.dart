a6bf054a0b0bb31df17ac58ed96ef9cf46842322
Zafar7645 <zafarshaikh645@gmail.com>
2023-01-05 11:34:56 +0530

    authentication

---
+++ lib/Screens/Authentication/signup.dart
@@ -0,0 +1,327 @@
+// import 'package:camera/camera.dart';
+import 'package:eye_suggest/Screens/Authentication/signin.dart';
+import 'package:eye_suggest/Screens/Home/home.dart';
+import 'package:flutter/material.dart';
+
+class Authentication extends StatelessWidget {
+  // final List<CameraDescription>? cameras;
+  Authentication({
+    Key? key,
+    // this.cameras,
+  }) : super(key: key);
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _canLogin(BuildContext context) {
+    FocusScope.of(context).unfocus();
+    var isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+    return isValid;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _email = "";
+    var _password = "";
+    var _name = "";
+    var _confirmPassword = "";
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.2,
+              // margin: const EdgeInsets.only(
+              //   top: 50.0,
+              // ),
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/images/app-bar.png'),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.8,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                  bottomLeft: Radius.circular(0),
+                  bottomRight: Radius.circular(0),
+                ),
+                color: Color.fromRGBO(196, 196, 196, 0.5400000214576721),
+              ),
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Register With Us!',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Your information is safe with us.',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Enter your full name',
+                              hintStyle: TextStyle(
+                                fontSize: 15,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromRGBO(33, 111, 182, 1),
+                                ),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your full name';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _name = value!;
+                            },
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Enter your email',
+                              hintStyle: TextStyle(
+                                fontSize: 15,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromRGBO(33, 111, 182, 1),
+                                ),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _email = value!;
+                            },
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Enter your password',
+                              hintStyle: TextStyle(
+                                fontSize: 15,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromRGBO(33, 111, 182, 1),
+                                ),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _password = value!;
+                            },
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Confirm password',
+                              hintStyle: TextStyle(
+                                fontSize: 15,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    20.0,
+                                  ),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromRGBO(33, 111, 182, 1),
+                                ),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please re-enter your password ';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _confirmPassword = value!;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: TextButton(
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                        fixedSize: MaterialStateProperty.all(
+                          Size.fromWidth(MediaQuery.of(context).size.width),
+                        ),
+                        foregroundColor: MaterialStateProperty.all(
+                          Colors.white,
+                        ),
+                        backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).primaryColor,
+                        ),
+                        elevation: MaterialStateProperty.all(2),
+                      ),
+                      onPressed: () {
+                        if (_canLogin(context)) {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const HomePage(
+                                  title: 'EyeSuggest',
+                                  // cameras: cameras,
+                                );
+                              },
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Already have an account?',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return SignIn();
+                              },
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Sign in',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 // import 'package:camera/camera.dart';
-// import 'package:eye_suggest/Screens/authentication.dart';
+// import 'package:eye_suggest/Screens/home.dart';
+// import 'package:eye_suggest/Screens/splash_screen.dart';
 
-import 'package:eye_suggest/Screens/home.dart';
+import 'dart:async';
+
+import 'package:eye_suggest/Screens/Authentication/signin.dart';
+import 'package:eye_suggest/Screens/Authentication/signup.dart';
 import 'package:flutter/material.dart';
 
 // late List<CameraDescription> _cameras;
@@ -24,19 +28,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<int, Color> color = {
+      50: const Color.fromRGBO(33, 111, 182, .1),
+      100: const Color.fromRGBO(33, 111, 182, .2),
+      200: const Color.fromRGBO(33, 111, 182, .3),
+      300: const Color.fromRGBO(33, 111, 182, .4),
+      400: const Color.fromRGBO(33, 111, 182, .5),
+      500: const Color.fromRGBO(33, 111, 182, .6),
+      600: const Color.fromRGBO(33, 111, 182, .7),
+      700: const Color.fromRGBO(33, 111, 182, .8),
+      800: const Color.fromRGBO(33, 111, 182, .9),
+      900: const Color.fromRGBO(33, 111, 182, 1),
+    };
+    MaterialColor themeColor = MaterialColor(0xFF216fb6, color);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Eye Suggest',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: themeColor,
+        fontFamily: 'ABeeZee',
       ),
-      home: const HomePage(
-        // cameras: cameras,
-        title: 'EyeSuggest',
-      ),
-      // home: Authentication(
-      //   cameras: cameras,
+      // home: const HomePage(
+      //   // cameras: cameras,
+      //   title: 'EyeSuggest',
       // ),
+      // home: StreamBuilder(
+      //     stream: FirebaseAuth.instance.authStateChanges(),
+      //     builder: (context, user) {
+      //       if (!user.hasData) {
+      //         return Authentication();
+      //       } else {
+      //         return SignIn();
+      //       }
+      //     }),
+      // home: const SplashscreenWidget(),
+      home: Authentication(),
     );
   }
 }
